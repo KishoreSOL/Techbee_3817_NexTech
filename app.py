c0982830ac72b3984a43c74517a7b37de5823948
Kishore B <kishoreyuva147@gmail.com>
2024-02-23 14:24:12 +0530

    kriya

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from langchain_core.runnables import RunnablePassthrough
 
 #function to load the vectordatabase
 def load_knowledgeBase():
-        embeddings=OpenAIEmbeddings(api_key="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")
+        embeddings=OpenAIEmbeddings(api_key="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")
         DB_FAISS_PATH = 'newvectorstore/db_faiss'
         db = FAISS.load_local(DB_FAISS_PATH, embeddings)
         return db
@@ -20,7 +20,7 @@ def load_knowledgeBase():
 #function to load the OPENAI LLM
 def load_llm():
         from langchain_openai import ChatOpenAI
-        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, api_key=("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"))
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, api_key=("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"))
         return llm
 
 #creating prompt template using langchain
@@ -29,7 +29,7 @@ def load_prompt():
         Given below is the context and question of the user.
         context = {context}
         question = {question}
-        if the answer is not in the pdf answer "reply that the content is not specified"
+        
          """
         prompt = ChatPromptTemplate.from_template(prompt)
         return prompt
@@ -37,6 +37,7 @@ def load_prompt():
 
 def format_docs(docs):
         return "\n\n".join(doc.page_content for doc in docs)
+
 
 
 if __name__=='__main__':
@@ -79,7 +80,7 @@ if __name__=='__main__':
         if(query):
                 #getting only the chunks that are similar to the query for llm to produce the output
                 similar_embeddings=knowledgeBase.similarity_search(query)
-                similar_embeddings=FAISS.from_documents(documents=similar_embeddings, embedding=OpenAIEmbeddings(api_key="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"))
+                similar_embeddings=FAISS.from_documents(documents=similar_embeddings, embedding=OpenAIEmbeddings(api_key="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"))
                 
                 #creating the chain for integrating llm,prompt,stroutputparser
                 retriever = similar_embeddings.as_retriever()
